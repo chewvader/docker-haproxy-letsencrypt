63ae1ae6bbf9bddf81e7306b237102013d6487bb
chewvader <chewvader@users.noreply.github.com>
2018-01-29 01:13:08 -0800

    Haproxy 1.7.10

--- certs.sh
+++ certs.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [ -n "$CERTS" ]; then
-    certbot certonly --no-self-upgrade -n --text --standalone \
+    certbot certonly --no-self-upgrade --http-01-port 30001 -n --text --standalone \
         --preferred-challenges http-01 \
         -d "$CERTS" --keep --expand --agree-tos --email "$EMAIL" \
         || exit 1
